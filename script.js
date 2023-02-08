dcd44e4874d140b020b42089314090111e67aa44
MOX UFO <mox.ufo@gmail.com>
2023-02-08 16:02:03 -0500

    add function to get length of password

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // Assignment code here
+function getLength(){
+  var passwordLength = prompt("What's the desired number of characters between 8-128 as an integer")
+  if(passwordLength >= 8 && passwordLength <= 128){
+    return passwordLength
+  } 
+  console.log("It's invalid")
+  return getLength()
+}
 
+
+
+function generatePassword(){
+  var passwordLength = getLength()
+  // console.log(passwordLength)
+  var randomNum = getNumChoices()
+  console.log(getNumChoices)
+  var password = ""
+  //generate it somehow
+  return password
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
