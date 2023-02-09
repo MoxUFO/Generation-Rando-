7df3865da2a8d680d3ab20caab99e496cfaf54c2
MOX UFO <mox.ufo@gmail.com>
2023-02-08 23:50:48 -0500

    updated a few javascrpit lines, main reason for push is to have updated version for askbcs

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ var upCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 var lowCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var specCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '/', '-', '+', '(', ')', '[', ']', '{', '}', '<', '>', '?', '~']
-var charGeneratorArr = []
+var charGeneratorArr = [upCase, getNumChoices]
+
+console.log(charGeneratorArr)
 
 
 
@@ -20,34 +22,39 @@ function getLength(){
 function getNumChoices(){
   var randomNum = Math.floor(Math.random() * numArr.length);
   var numChoice = numArr[randomNum];
+  // console.log(numChoice)
   return numChoice;
 }
 
 function upCase(){ 
   var randomUpCase = Math.floor(Math.random() * upCaseArr.length)
  var upChoice = upCaseArr[randomUpCase];
+//  console.log(upChoice)
  return upChoice;
 }
 
 function lowCase(){
   randomLowCase = Math.floor(Math.random() * lowCaseArr.length);
   var lowChoice = lowCaseArr[randomLowCase];
+  // console.log(lowChoice)
   return lowChoice;
 }
 
 function specCharacters(){
   randomSpecChar = Math.floor(Math.random() * specCharArr.length);
   var specChoice = specCharArr[randomSpecChar];
+  // console.log(specChoice)
   return specChoice
 }
 
 function generatePassword(){
-  var passwordLength = getLength()
+  passwordLength = getLength()
   console.log(passwordLength)
-  var numChoice = getNumChoices()
-  var upChoice= upCase()
-  var lowChoice = lowCase()
-  var specChoice = specCharacters()
+   numChoice = getNumChoices()
+   console.log(numChoice)
+   upChoice= upCase()
+   lowChoice = lowCase()
+   specChoice = specCharacters()
 
 
   //generate it somehow
