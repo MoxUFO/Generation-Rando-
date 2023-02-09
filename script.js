1bbb47d5b27fd1e9a34a7664451690a27a6fbf5c
MOX UFO <mox.ufo@gmail.com>
2023-02-08 19:05:14 -0500

    added upper and lower case number generator

--- script.js
+++ script.js
@@ -13,19 +13,41 @@ function getNumChoices(){
   var randomNum = Math.floor(Math.random() * numArr.length)
   var numChoice =  confirm( 'include Numbers?');
     if (numChoice === true) {
+      console.log(randomNum)
       return randomNum;
     }
     console.log("It's Invalid")
     return null;
 }
 
+function upCase(){
+  var upCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P' ,'Q', 'R', 'S', 'T' , 'U', 'V', 'W', 'X', 'Y', 'Z']
+  var randomUpCase = Math.floor(Math.random() * upCaseArr.length)
+  var upCasePromt = confirm('Include Upper case Letters?');
+  if ( upCasePromt === true){
+  console.log(upCaseArr[randomUpCase])
+  }
+}
+
+function lowCase(){
+  var lowCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  randonLowCase = Math.floor(Math.random() * lowCaseArr.lengthv)
+  var lowCasePromt = confirm('Include Lower case Letters?')
+  if (lowCasePromt === true){
+    console.log(lowCaseArr[randonLowCase])
+  }
+}
+
+function specCharacters(){
+
+}
 
 function generatePassword(){
   var passwordLength = getLength()
   // console.log(passwordLength)
   var randomNum = getNumChoices()
-  console.log(randomNum)
-  var password = ""
+  // console.log(randomNum)
+  var randomUpCase = upCase()
   //generate it somehow
   return password
 }
